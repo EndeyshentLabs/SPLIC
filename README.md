b7c92de4a02d776281525181965d98d2d4a1a718
EndeyshentLabs <Themikfound@gmail.com>
2023-04-11 17:05:42 +0300

    Added CHAR-NL; Made code more readable; Small doc in `README.md`

--- README.md
+++ README.md
@@ -42,3 +42,18 @@ To run the intrepreter use:
 $ clojure splic.clj
 <program_output>
 ```
+
+## Reference
+
+| CodeName | Symbol | Function |
+| --       | --     | -- |
+| PLUS     | +      | Sum up two numbers |
+| MINUS    | -      | Subtract one number from another |
+| MULT     | *      | Multiply one number by another |
+| DIV      | /      | Divide one number by another |
+| DUMP     | !      | Print value on top of the stack |
+| CHAR     | @      | Print value on top of the stack as ASCII character |
+| CHAR-NL  | @:     | Print value on top of the stack as ASCII character but whith new line at the end |
+| BREAK    | #      | Exit from program |
+| HALT     | !#     | Halt the program(exit with 1) |
+| DEBUG    | \|     | Dump all stack |
--- splic.clj
+++ splic.clj
@@ -1,3 +1,5 @@
+(require '[clojure.string])
+
 (def spl-keywords {
   :PLUS "+",
   :MINUS "-",
@@ -5,6 +7,7 @@
   :DIV "/",
   :DUMP "!",
   :CHAR "@",
+  :CHAR-NL "@:",
   :BREAK "#",
   :HALT "!#",
   :DEBUG "|"
@@ -25,7 +28,11 @@
 
 (defn make-kwd [id] (kwd id))
 (defn make-number [n] (number n))
-(defn make-error [i] (printf "SPLIC.ERROR Unknown expresssion: `%s`.\n" i) (println "  \033[3mGood luck trying to find where this error happened.\033[0m") (System/exit 1))
+(defn make-error [i]
+  (printf  "SPLIC.ERROR Unknown expresssion: `%s`.\n" i)
+  (println "  \033[3mGood luck trying to find where this error happened.\033[0m")
+  (System/exit 1)
+)
 
 (doseq [i words]
   (condp = (clojure.string/lower-case (clojure.string/trim i))
@@ -35,10 +42,12 @@
     (spl-kwd :DIV)   (make-kwd i)
     (spl-kwd :DUMP)  (make-kwd i)
     (spl-kwd :CHAR)  (make-kwd i)
+    (spl-kwd :CHAR-NL)  (make-kwd i)
     (spl-kwd :DEBUG)  (make-kwd i)
     (spl-kwd :BREAK) (make-kwd i)
     (spl-kwd :HALT) (make-kwd i)
     (re-matches #"\d*" i) (make-number i)
+    (re-matches #"-\d*" i) (make-number i)
     (make-error i)
   )
 )
@@ -46,7 +55,9 @@
 (defn drop-nth [n coll]
   (concat
     (take n coll)
-    (drop (inc n) coll)))
+    (drop (inc n) coll)
+  )
+)
 
 (doseq [i spl-stack]
   (condp = i
@@ -72,17 +83,50 @@
                            (def spl-stack (drop-nth 1 spl-stack))
                            (def spl-stack (drop-nth 1 spl-stack))
                          )
-    (spl-kwd :MULT) :>> (fn [_] (number* (* (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))) (read-string (nth spl-stack (- (.indexOf spl-stack i) 2))))) (def spl-stack (drop-nth 1 spl-stack)) (def spl-stack (drop-nth 1 spl-stack)) (def spl-stack (drop-nth 1 spl-stack)))
-    (spl-kwd :DIV) :>> (fn [_] (number* (* (read-string (nth spl-stack (- (.indexOf spl-stack i) 2))) (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))))) (def spl-stack (drop-nth 1 spl-stack)) (def spl-stack (drop-nth 1 spl-stack)) (def spl-stack (drop-nth 1 spl-stack)))
-    (spl-kwd :DUMP) :>> (fn [_] (println (nth spl-stack (- (.indexOf spl-stack i) 1))) (def spl-stack (drop 2 spl-stack)))
-    (spl-kwd :CHAR) :>> (fn [_] (println (char (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))))) (def spl-stack (drop 2 spl-stack)))
+    (spl-kwd :MULT) :>> (fn [_]
+                          (number*
+                            (* (read-string (nth spl-stack (- (.indexOf spl-stack i) 1)))
+                               (read-string (nth spl-stack (- (.indexOf spl-stack i) 2))))
+                          )
+                          (def spl-stack (drop-nth 1 spl-stack))
+                          (def spl-stack (drop-nth 1 spl-stack))
+                          (def spl-stack (drop-nth 1 spl-stack))
+                        )
+    (spl-kwd :DIV) :>> (fn [_]
+                         (number*
+                           (* (read-string (nth spl-stack (- (.indexOf spl-stack i) 2)))
+                              (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))))
+                         )
+                         (def spl-stack (drop-nth 1 spl-stack))
+                         (def spl-stack (drop-nth 1 spl-stack))
+                         (def spl-stack (drop-nth 1 spl-stack))
+                       )
+    (spl-kwd :DUMP) :>> (fn [_]
+                          (println (nth spl-stack (- (.indexOf spl-stack i) 1)))
+                          (def spl-stack (drop 2 spl-stack))
+                        )
+    (spl-kwd :CHAR) :>> (fn [_]
+                          (print
+                            (char (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))))
+                          )
+                          (def spl-stack (drop 2 spl-stack))
+                        )
+    (spl-kwd :CHAR-NL) :>> (fn [_]
+                          (println
+                            (char (read-string (nth spl-stack (- (.indexOf spl-stack i) 1))))
+                          )
+                          (def spl-stack (drop 2 spl-stack))
+                        )
     (spl-kwd :BREAK) :>> (fn [_]
                            (System/exit 0)
                          )
     (spl-kwd :HALT) :>> (fn [_]
                           (System/exit 1)
                         )
-    (spl-kwd :DEBUG) :>> (fn [_] (println spl-stack) (def spl-stack (drop 1 spl-stack)))
-    (wait)
+    (spl-kwd :DEBUG) :>> (fn [_]
+                           (println spl-stack)
+                           (def spl-stack (drop 1 spl-stack))
+                         )
+    (wait) ;; Fall through
   )
 )
